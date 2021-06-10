6976b30adcb120620a1f103bdc5ae13e692942b5
Maria DelSignore <maria.ad27@gmail.com>
2021-06-10 12:25:27 -0600

    Create variables for gameboard and squares

---
+++ src/main.js
@@ -0,0 +1,16 @@
+// QUERYSELECTORS
+var gameBoard = document.getElementById('centerContainer');
+var square1 = document.getElementById('one');
+var square2 = document.getElementById('two');
+var square3 = document.getElementById('three');
+var square4 = document.getElementById('four');
+var square5 = document.getElementById('five');
+var square6 = document.getElementById('six');
+var square7 = document.getElementById('seven');
+var square8 = document.getElementById('eight');
+var square9 = document.getElementById('nine');
+
+
+// BUTTONS
+
+// EVENTLISTENERS

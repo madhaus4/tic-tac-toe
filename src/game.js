d7a57550b8577069ec193adb24ce4d1c1e67b834
Maria DelSignore <maria.ad27@gmail.com>
2021-06-10 16:45:07 -0600

    Tell player/game story

--- src/game.js
+++ src/game.js
@@ -9,42 +9,46 @@ class Game {
   }
 
   winConditions() {
-    // winning combinations
-      // 123
-        ['sq1', 'sq2', 'sq3']
-      // 456
-        ['sq4', 'sq5', 'sq6']
-      // 789
-        ['sq7', 'sq8', 'sq9']
-      // 147
-        ['sq1', 'sq4', 'sq7']
-      // 258
-        ['sq2', 'sq5', 'sq8']
-      // 369
-        ['sq3', 'sq6', 'sq9']
-      // 159 or 951
-        ['sq1', 'sq5', 'sq9']
-        ['sq9', 'sq5', 'sq1']
-      // 357 or 753
-        ['sq3', 'sq5', 'sq7']
-        ['sq7', 'sq5', 'sq3']
+    if (['sq1', 'sq2', 'sq3'] || ['sq4', 'sq5', 'sq6'] ||
+      ['sq7', 'sq8', 'sq9'] || ['sq1', 'sq4', 'sq7'] ||
+      ['sq2', 'sq5', 'sq8'] || ['sq3', 'sq6', 'sq9'] ||
+      ['sq1', 'sq5', 'sq9'] || ['sq3', 'sq5', 'sq7']) {
+      this.hasWinner = true;
+    }
 
   }
 }
 
+// *****  I AM A PLAYER  *****
+  // who goes first???
+    // new games start with player1 but if its a persisting game, whoever has won the previous?
 
-// whose turn is it
+  // I click a square, my token appears
+      // board title changes to say it's the other player's   turn
+      // then other user selects a square
+        // board title changes to say it's the other player's turn
+        // repeat until game is won or there is a draw
+
+    // when a game is won, 'it's ___ turn!', will display 'it's "winners" turn!'
+      // 'wins' will update on player who won
+      // game board will automatically reset after a few seconds
+
+    // if game is a draw, no one wins,  board title will say it's a draw, then reset after a frew seconds
+
+// *****  DETERMINING A WINNER  *****
 
 
+// ***** WHOE TURN IS IT?  *****
 
-// what data do you want to keep track of
-  // whose turn is it
-  // whats the score
 
-// what o's are out and where are they
-// what x's are out and where are they
+// *****  KEEPING TRACK OF SCORE  *****
+
+
+// *****  HOW MANY TOKENS ARE ON THE BOARD  *****
+  // what o's are out and where are they
+  // what x's are out and where are they
+
 
 // game board data
-// what is my approach
 
-//
+// *****  WHAT IS MY APPROACH?????  *****

a7405e3a7ce23a50a140bb6192582296454ad5c4
Maria DelSignore <maria.ad27@gmail.com>
2021-06-13 13:21:25 -0600

    Update check for draw conditional

--- src/game.js
+++ src/game.js
@@ -15,9 +15,7 @@ class Game {
     ];
   }
 
-// *****  DETERMINING A WINNER  *****
   collectSquares(player) {
-    // this adds a square ID into the player's selections array
     this.counter += 1;
     for (var i = 0; i < this.gameBoard.length; i ++) {
       if (this.gameBoard[i] === event.target.id) {
@@ -27,27 +25,24 @@ class Game {
   }
 
   checkforWin() {
-    // this checks if the player's selections includes a winning combination & updates hasWinner to true & increases players wins
     for (var i = 0; i < this.winCombinations.length; i++) {
       var combination = this.winCombinations[i];
       if (combination.every(elem => this.currentPlayer.selections.includes(elem))) {
         this.hasWinner = true;
         this.currentPlayer.wins += 1;
         console.log(this.hasWinner);
-        // console.log('player', this.currentPlayer);
-        // console.log('wins', this.currentPlayer.wins);
       }
     }
   }
 
   checkForDraw() {
-    if (!this.hasWinner) {
+    if (!this.hasWinner && this.counter === 9) {
       this.isDraw = true;
     }
+    // this.resetGameBoard();
   }
 
   resetGameBoard() {
-  // when you add a winner property >> reset winner****
     this.player1.selections = [];
     this.player2.selections = [];
     this.currentPlayer = this.player1;
@@ -56,46 +51,7 @@ class Game {
     this.counter = 0;
   }
 
-
-
-
-  //   for (var i = 0; i < this.winCombinations.length; i++) {
-  //     var combo = this.winCombinations[i];
-  //     for (var i = 0; i < combo.length; i++) {
-  //       if (this.currentPlayer.selections.includes(combo[i]) && this.currentPlayer.selections.includes(combo[(i + 1)]) &&
-  //       this.currentPlayer.selections.includes(combo[(i + 2)])) {
-  //         this.hasWinner = true;
-  //       }
-  //     }
-  //     return;
-  //   }
-  // }
-        // if (!combo.includes(this.currentPlayer.selections[i])) {
-        // } else
-  //       if (combo.includes(this.currentPlayer.selections[i])) {
-  //          // && this.counter === 2) {
-  //         // this.hasWinner = true;
-  //       }
-  //       // else
-  //       if (this.counter === 3) {
-  //         this.counter = 0;
-  //       }
-  //     }
-  //     return;
-  //   }
-  // }
-      // var stringedSquares = this.currentPlayer.selections
-      // .toString();
-      // I think I really want the reverse logic >> if selections includes a win combo...
-      // if (stringedSquares.includes(combo)) {
-      //   this.hasWinner = true;
-      //   this.currentPlayer.wins += 1;
-
-
-
-// ***** WHOSE TURN IS IT?  *****
   updatePlayerTurn() {
-    // this changes players turn from true to false and false to true
     if (this.player1.turn) {
       this.player1.turn = false;
       this.player2.turn = true;
@@ -110,20 +66,12 @@ class Game {
   updateCurrentPlayer(player) {
     this.currentPlayer = player;
   }
+}
 
-  // *****  KEEPING TRACK OF SCORE  *****
-  updateScore(player) {
-    if (this.hasWinner) {
-      player.wins += 1;
-    }
-  }
 
   // *****  HOW MANY TOKENS ARE ON THE BOARD  *****
   // what o's are out and where are they
   // what x's are out and where are they
-
-}
-// game board data
 
 // *****  WHAT IS MY APPROACH?????  *****
 
@@ -135,3 +83,39 @@ class Game {
   // when a user selects a square, I increment turn's taken by each player and tell the other player it's their turn
 
   // when a player has a winning combination, trigger some DOM shit, then reset the board & update player wins
+
+
+
+
+
+    //   for (var i = 0; i < this.winCombinations.length; i++) {
+    //     var combo = this.winCombinations[i];
+    //     for (var i = 0; i < combo.length; i++) {
+    //       if (this.currentPlayer.selections.includes(combo[i]) && this.currentPlayer.selections.includes(combo[(i + 1)]) &&
+    //       this.currentPlayer.selections.includes(combo[(i + 2)])) {
+    //         this.hasWinner = true;
+    //       }
+    //     }
+    //     return;
+    //   }
+    // }
+          // if (!combo.includes(this.currentPlayer.selections[i])) {
+          // } else
+    //       if (combo.includes(this.currentPlayer.selections[i])) {
+    //          // && this.counter === 2) {
+    //         // this.hasWinner = true;
+    //       }
+    //       // else
+    //       if (this.counter === 3) {
+    //         this.counter = 0;
+    //       }
+    //     }
+    //     return;
+    //   }
+    // }
+        // var stringedSquares = this.currentPlayer.selections
+        // .toString();
+        // I think I really want the reverse logic >> if selections includes a win combo...
+        // if (stringedSquares.includes(combo)) {
+        //   this.hasWinner = true;
+        //   this.currentPlayer.wins += 1;

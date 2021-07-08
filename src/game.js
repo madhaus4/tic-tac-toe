5cf2e783a7e885b3a5458f8db3388e1a834c5621
Maria DelSignore <maria.ad27@gmail.com>
2021-07-08 17:39:07 -0600

    Update syntax

--- src/game.js
+++ src/game.js
@@ -1,73 +1,78 @@
 class Game {
-  constructor() {
-    this.player1 = new Player('1', `<p class="board-title-player board-token token1">C</p>`, true);
-    this.player2 = new Player('2', `<p class="board-title-player board-token token2">Y</p>`, false);
-    this.currentPlayer = this.player1;
-    this.counter = 0;
-    this.hasWinner = false;
-    this.isDraw = false;
-    this.gameBoard = ['sq1', 'sq2', 'sq3', 'sq4', 'sq5', 'sq6', 'sq7', 'sq8', 'sq9'];
-    this.winCombinations = [
-      ['sq1', 'sq2', 'sq3'], ['sq4', 'sq5', 'sq6'],
-      ['sq7', 'sq8', 'sq9'], ['sq1', 'sq4', 'sq7'],
-      ['sq2', 'sq5', 'sq8'], ['sq3', 'sq6', 'sq9'],
-      ['sq1', 'sq5', 'sq9'], ['sq3', 'sq5', 'sq7']
-    ];
-  }
-
-  collectSquares(player) {
-    this.counter++;
-    for (var i = 0; i < this.gameBoard.length; i ++) {
-      if (this.gameBoard[i] === event.target.id) {
-        player.selections.push(this.gameBoard[i]);
-      }
+    constructor() {
+        this.player1 = new Player('1', `<p class="board-title-player board-token token1">C</p>`, true);
+        this.player2 = new Player('2', `<p class="board-title-player board-token token2">Y</p>`, false);
+        this.currentPlayer = this.player1;
+        this.counter = 0;
+        this.hasWinner = false;
+        this.isDraw = false;
+        this.gameBoard = ['sq1', 'sq2', 'sq3', 'sq4', 'sq5', 'sq6', 'sq7', 'sq8', 'sq9'];
+        this.winCombinations = [
+            ['sq1', 'sq2', 'sq3'],
+            ['sq4', 'sq5', 'sq6'],
+            ['sq7', 'sq8', 'sq9'],
+            ['sq1', 'sq4', 'sq7'],
+            ['sq2', 'sq5', 'sq8'],
+            ['sq3', 'sq6', 'sq9'],
+            ['sq1', 'sq5', 'sq9'],
+            ['sq3', 'sq5', 'sq7']
+        ];
     }
-  }
 
-  updateGameBoard() {
-    for (var i = 0; i < this.gameBoard.length; i ++) {
-      if (this.gameBoard[i] === event.target.id) {
-        this.gameBoard.splice(i, 1);
-      }
+    collectSquares(player, event) {
+        this.counter++;
+        this.gameBoard.filter(square => {
+            if (square === event.target.id) {
+                player.selections.push(square)
+            };
+        });
     }
-  }
 
-  checkforWin() {
-    for (var i = 0; i < this.winCombinations.length; i++) {
-      var combination = this.winCombinations[i];
-      if (combination.every(elem => this.currentPlayer.selections.includes(elem))) {
-        this.hasWinner = true;
-        this.updateWins();
-      }
+    updateGameBoard(event) {
+        this.gameBoard.map(square => {
+            if (square === event.target.id) {
+                let i = this.gameBoard.indexOf(square);
+                this.gameBoard.splice(i, 1);
+            }
+        });
     }
-  }
 
-  checkForDraw() {
-    if (!this.hasWinner && this.counter === 9) {
-      this.isDraw = true;
+    checkforWin() {
+        for (var i = 0; i < this.winCombinations.length; i++) {
+            var combination = this.winCombinations[i];
+            if (combination.every(elem => this.currentPlayer.selections.includes(elem))) {
+                this.hasWinner = true;
+                this.updateWins();
+            }
+        }
     }
-  }
 
-  updateWins() {
-    if (this.hasWinner) {
-      this.currentPlayer.wins += 1;
-      this.currentPlayer.saveWinsToStorage();
+    checkForDraw() {
+        if (!this.hasWinner && this.counter === 9) {
+            this.isDraw = true;
+        }
     }
-  }
 
-  updatePlayerTurn() {
-    if (this.player1.turn) {
-      this.player1.turn = false;
-      this.player2.turn = true;
-      this.updateCurrentPlayer(this.player2);
-    } else if (this.player2.turn) {
-      this.player2.turn = false;
-      this.player1.turn = true;
-      this.updateCurrentPlayer(this.player1);
+    updateWins() {
+        if (this.hasWinner) {
+            this.currentPlayer.wins += 1;
+            this.currentPlayer.saveWinsToStorage();
+        }
     }
-  }
 
-  updateCurrentPlayer(player) {
-    this.currentPlayer = player;
-  }
+    updatePlayerTurn() {
+        if (this.player1.turn) {
+            this.player1.turn = false;
+            this.player2.turn = true;
+            this.updateCurrentPlayer(this.player2);
+        } else if (this.player2.turn) {
+            this.player2.turn = false;
+            this.player1.turn = true;
+            this.updateCurrentPlayer(this.player1);
+        }
+    }
+
+    updateCurrentPlayer(player) {
+        this.currentPlayer = player;
+    }
 }
--- src/main.js
+++ src/main.js
@@ -18,104 +18,108 @@ var soundEffect = document.getElementById('soundEffect');
 var currentGame = new Game();
 
 window.addEventListener('load', getWinsFromStorage);
-gameBoard.addEventListener('click', runGame);
 newPlayBtn.addEventListener('click', newGame);
-gameBoard.addEventListener('mouseenter', function() {
-  if (currentGame.counter === 0 || currentGame.counter === 1) {
-    soundEffect.play();
-  }
+gameBoard.addEventListener('mouseenter', playSound);
+gameBoard.addEventListener('click', function(event) {
+    runGame(event)
 });
 
-function runGame() {
-  currentGame.collectSquares(currentGame.currentPlayer);
-  currentGame.checkforWin();
-  currentGame.checkForDraw();
-  renderToken();
-  currentGame.updateGameBoard();
-  disableClick();
-  updateBoardTitle();
-  resetGame();
-  getWinsFromStorage();
+function playSound() {
+    if (currentGame.counter === 0 || currentGame.counter === 1) {
+        soundEffect.play();
+    }
+}
+
+function runGame(event) {
+    currentGame.collectSquares(currentGame.currentPlayer, event);
+    currentGame.checkforWin();
+    currentGame.checkForDraw();
+    renderToken();
+    currentGame.updateGameBoard(event);
+    disableClick();
+    updateBoardTitle();
+    resetGame();
+    getWinsFromStorage();
 }
 
 function renderToken() {
-  var idTarget = event.target.id;
-  for (var i = 0; i < currentGame.gameBoard.length; i++) {
-    if (currentGame.gameBoard[i] === idTarget && currentGame.gameBoard.includes(idTarget)) {
-      eval(idTarget).innerHTML = currentGame.currentPlayer.token;
-      currentGame.updatePlayerTurn();
+    var idTarget = event.target.id;
+    for (var i = 0; i < currentGame.gameBoard.length; i++) {
+        if (currentGame.gameBoard[i] === idTarget && currentGame.gameBoard.includes(idTarget)) {
+            eval(idTarget).innerHTML = currentGame.currentPlayer.token;
+            currentGame.updatePlayerTurn();
+        }
     }
-  }
-  currentGame.checkforWin();
-  currentGame.checkForDraw();
-  updateBoardTitle();
+    currentGame.checkforWin();
+    currentGame.checkForDraw();
+    updateBoardTitle();
 }
 
 function updateBoardTitle() {
-  if (!currentGame.hasWinner && currentGame.player1.turn) {
-    boardTitle.innerHTML = `<p>It's</p><p class="board-title-player token1">C</p><p>'s turn!</p>`;
-  } else if (!currentGame.hasWinner && currentGame.player2.turn) {
-    boardTitle.innerHTML = `<p>It's</p><p class="board-title-player token2">Y</p><p>'s turn!</p>`;
-  }
+    if (!currentGame.hasWinner && currentGame.player1.turn) {
+        boardTitle.innerHTML = `<p>It's</p><p class="board-title-player token1">C</p><p>'s turn!</p>`;
+    } else if (!currentGame.hasWinner && currentGame.player2.turn) {
+        boardTitle.innerHTML = `<p>It's</p><p class="board-title-player token2">Y</p><p>'s turn!</p>`;
+    }
 
-  if (currentGame.hasWinner && !currentGame.player1.turn) {
-    boardTitle.innerHTML = `<p class="board-title-player token1">C</p><p> WINS!!!</p>`;
-  } else if (currentGame.hasWinner && !currentGame.player2.turn) {
-    boardTitle.innerHTML = `<p class="board-title-player token2">Y</p><p> WINS!!!</p>`;
-  }
+    if (currentGame.hasWinner && !currentGame.player1.turn) {
+        boardTitle.innerHTML = `<p class="board-title-player token1">C</p><p> WINS!!!</p>`;
+    } else if (currentGame.hasWinner && !currentGame.player2.turn) {
+        boardTitle.innerHTML = `<p class="board-title-player token2">Y</p><p> WINS!!!</p>`;
+    }
 
-  if (currentGame.isDraw && currentGame.counter <= 10 && !currentGame.hasWinner) {
-    boardTitle.innerHTML = `<p>It\'s a draw! Play again!</p>`;
-  }
+    if (currentGame.isDraw && currentGame.counter <= 10 && !currentGame.hasWinner) {
+        boardTitle.innerHTML = `<p>It\'s a draw! Play again!</p>`;
+    }
 }
 
 function resetGame() {
-  if (currentGame.hasWinner || currentGame.hasDraw || currentGame.counter === 9) {
-    timeOut();
-  }
+    if (currentGame.hasWinner || currentGame.hasDraw || currentGame.counter === 9) {
+        timeOut();
+    }
 }
 
 function timeOut() {
-  setTimeout(function() {
-    clearGameBoard();
-    currentGame = new Game();
-    boardTitle.innerHTML = `
+    setTimeout(function() {
+        clearGameBoard();
+        currentGame = new Game();
+        boardTitle.innerHTML = `
       <p class="board-title-player token1">C</p>
       <p>, kick the game off!</p>`;
-    enableClick();
-  }, 2000);
+        enableClick();
+    }, 2000);
 }
 
 function getWinsFromStorage() {
-  currentGame.player1.retrieveWinsFromStorage();
-  currentGame.player2.retrieveWinsFromStorage();
-  displayWins();
+    currentGame.player1.retrieveWinsFromStorage();
+    currentGame.player2.retrieveWinsFromStorage();
+    displayWins();
 }
 
 function displayWins() {
-  p1Wins.innerText = currentGame.player1.wins;
-  p2Wins.innerText = currentGame.player2.wins;
+    p1Wins.innerText = currentGame.player1.wins;
+    p2Wins.innerText = currentGame.player2.wins;
 }
 
 function clearGameBoard() {
-  for (var i = 0; i < gameSpaces.length; i++) {
-    gameSpaces[i].innerHTML = '';
-  }
+    for (var i = 0; i < gameSpaces.length; i++) {
+        gameSpaces[i].innerHTML = '';
+    }
 }
 
 function disableClick() {
-  if (currentGame.hasWinner || currentGame.counter === 9) {
-    gameBoard.removeEventListener('click', runGame);
-  }
+    if (currentGame.hasWinner || currentGame.counter === 9) {
+        gameBoard.removeEventListener('click', runGame);
+    }
 }
 
 function enableClick() {
-  if (!currentGame.hasWinner) {
-    gameBoard.addEventListener('click', runGame);
-  }
+    if (!currentGame.hasWinner) {
+        gameBoard.addEventListener('click', runGame);
+    }
 }
 
 function newGame() {
-  localStorage.clear();
-  displayWins();
+    localStorage.clear();
+    displayWins();
 }
